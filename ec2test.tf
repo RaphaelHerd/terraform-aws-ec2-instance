c3a8bcde8086f290700a9ea09d8b7467992a7d53
Raphael Herding <raphael.herding@bayer.com>
2018-12-04 15:54:00 +0100

    change

--- ec2test.tf
+++ ec2test.tf
@@ -1,8 +1,7 @@
 provider "aws" {
-  region                  = "eu-central-1"
-  shared_credentials_file = "../.aws/credentials"
-
-  #  profile = "terraform"
+  region = "${var.region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
 
 resource "aws_instance" "example" {
